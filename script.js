1f2b85e28df332e2554757dc8454268dcfa2110e
Ikuya Nagaki <ikuya313198@gmail.com>
2024-01-23 16:17:24 +0900

    add script

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("learn").addEventListener("click", function () {
+  alert("すみません このページはまだ作ってません");
+});
+
+document.getElementById("doc").addEventListener("click", function () {
+  alert("このページはそのうち作ります");
+});
+
+document.getElementById("com").addEventListener("click", function () {
+  alert("押してくれてありがとう");
+});
